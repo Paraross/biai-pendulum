fdefcb44459ca1d00f8063b470b442f2a8e54428
Paraross <Adrago@onet.pl>
2025-05-14 08:34:26 +0200

    Add options to print info

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 import gymnasium as gym
 
+print_to_console = False
+write_to_file = True
+
+def print_info_to_console(current_step, action, observation, reward):
+    print(f"step        : {current_step}")
+    print(f"action      : {action}")
+    print(f"observation : {observation}")
+    print(f"reward      : {reward}\n")
+
+def write_info_to_file(file, current_step, action, observation, reward):
+    file.write(f"step        : {current_step}\n")
+    file.write(f"action      : {action}\n")
+    file.write(f"observation : {observation}\n")
+    file.write(f"reward      : {reward}\n\n")
+
 def main():
-    env = gym.make("Pendulum-v1", render_mode="human", g=9.81)
+    env = gym.make("Pendulum-v1", render_mode="rgb_array", g=9.81)
 
     observation, info = env.reset()
+
+    file = open("output.txt", "w" if write_to_file else "a")
     
-    max_steps = 500
+    max_steps = 100
     current_step = 0
     episode_over = False
     while not episode_over and current_step < max_steps:
         action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
         env.render()
+
+        if print_to_console:
+            print_info_to_console(current_step, action, observation, reward)
+        if write_to_file:
+            write_info_to_file(file, current_step, action, observation, reward)
+        
+        if reward > -1.0:
+            print(f"Reward is good, step: {current_step}\n")
+        
+
+        episode_over = terminated or truncated
         current_step += 1
 
-    episode_over = terminated or truncated
-
+    file.close()
     env.close()
 
 if __name__ == "__main__":
